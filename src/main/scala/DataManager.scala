27248f639c146c41048659071cb5af8f5554b6f0
albgut <alban.gutierrez-andre@etudiant.univ-rennes1.fr>
2022-01-17 12:31:49 +0100

    dezip test leture des data

--- src/main/scala/DataManager.scala
+++ src/main/scala/DataManager.scala
@@ -1,3 +1,8 @@
-class DataManager {
+import java.nio.file.{Files, Paths}
 
+object DataManager extends App{
+  //val test_labels = scala.io.Source.fromFile("data/t10k-labels.idx1-ubyte")
+  val truc = Files.readAllBytes(Paths.get("data/t10k-labels.idx1-ubyte"))
+  //test_labels.getLines().foreach(println)
+  truc.take(10).foreach(x => println(x.toBinaryString))
 }
